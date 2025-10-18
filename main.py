70b099f18b08e94b8ad9021395c47cbbd25b3686
taconoodle <giannisvlassis1@gmail.com>
2025-10-19 01:19:13 +0300

    Added a function to create a list with the average speeds of the robots

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def avg_speed(min_speed, max_speed):
             if robot_id not in robot_info:
                 robot_info[robot_id] = [0, 0]
 
+            #GIANNHS: Des mhpws se symferei na diaireis me th mia me to count kai na vazeis to average sti lista
             robot_info[robot_id][0] += speed
             robot_info[robot_id][1] += 1
 
@@ -32,12 +33,46 @@ def avg_speed(min_speed, max_speed):
         avg = sum_speed / count
         if min_speed < avg <= max_speed:
             print(f"{robot_id} {avg} {count}")
+    #GIANNHS: Na epistrefoume th lista gia na th xrhsimopoioume
     return 1
 
 
 ########################## TACO CODE #############################
 
-def top_speed():
+datapath = "C:/Users/giann/Downloads/smart_factory_robots.csv"
+def get_avg_speeds():
+    #Dictionary of the form: robot_info[robot ID] = [cumulative speed, count of measurements]
+    robot_info = {}
+
+    with open(datapath) as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            #Get the data from the datafile
+            robot_id = int(row[0])
+            vx = float(row[6])
+            vy = float(row[7])
+
+            #Given formula
+            speed = math.sqrt(vx ** 2 + vy ** 2)
+
+            #If we don't initialize, the addition under will fail
+            if robot_id not in robot_info:
+                robot_info[robot_id] = [0, 0]
+
+            robot_info[robot_id][0] += speed
+            robot_info[robot_id][1] += 1
+
+    #Dictionary of the form: robot_avgs[robot ID] = [avg speed, count of measurements]
+    robot_avgs = {}
+    for robot_id, (sum_speed, count) in robot_info.items():
+        avg = sum_speed / count
+        robot_avgs[robot_id] = [avg, count]
+
+    return robot_avgs
+
+# def top_speed(robot_count):
+
 
 
 ######################## GENERAL CODE ############################
